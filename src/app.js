de309c77d8b1881a09c96fddd4fae0e5469ee4d9
cephalopodix <125798676+cephalopodix@users.noreply.github.com>
2023-03-21 22:10:54 +0100

    javascript update
    Showing the current temperature, humidity and wind speed of London

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+function displayTemperature(response) {
+  console.log(response.data.city);
+  console.log(response.data.temperature.current);
+  console.log(response.data.condition.description);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#percipitation");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "4odc8139abt32519f0708beaafc0f3a7";
+let city = "London";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
