f7871ec8e969c08d7b7466102ec5d541a7de9202
cephalopodix <125798676+cephalopodix@users.noreply.github.com>
2023-03-21 22:40:15 +0100

    console log removed and icon updated

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 function formatDate(timestamp) {
-  console.log(timestamp);
   let date = new Date(timestamp * 1000);
-  console.log(date);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -31,17 +29,20 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#percipitation");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time);
+  iconElement.setAttribute("src", response.data.condition.icon_url);
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "4odc8139abt32519f0708beaafc0f3a7";
-let city = "London";
+let city = "New York";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
-console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
